02ee3b0ed58c42dc1652b07f86ed19b7d9e7c586
YaroslavAnt <antonchyk.yar@gmail.com>
2019-05-22 12:23:49 +0300

    test

---
+++ build/precache-manifest.67e229e0dc302189fe2e92ecee7f0004.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f42669ddf30d7239d1111eccfb009aba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa222fdf17d2c2119eab",
+    "url": "/static/css/main.c746be8b.chunk.css"
+  },
+  {
+    "revision": "64f1242e8f65e1580c73",
+    "url": "/static/js/2.56125fc1.chunk.js"
+  },
+  {
+    "revision": "aa222fdf17d2c2119eab",
+    "url": "/static/js/main.59142cba.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
